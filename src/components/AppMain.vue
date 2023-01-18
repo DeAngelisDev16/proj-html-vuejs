a3a6e763d76dfc5ac63898e7d1dac8604bcab278
Shelbot77 <alessiodeangelis021@gmail.com>
2023-01-18 16:33:03 +0100

    new_courses section struttura macroblocchi commit

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -8,14 +8,53 @@ export default {
 </script>
 
 <template>
-    <div class="jumbo">
+    <section class="jumbo">
         <h2>jumbo section</h2>
-    </div>
-    <div class="founder_info">
+    </section>
+
+    <section class="founder_info">
         <div class="container">
             <h2>founder info section</h2>
         </div>
-    </div>
+    </section>
+
+    <section class="new_courses">
+        <div class="courses_container">
+            <div class="course_card">
+                <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat quidem nisi cupiditate veniam?
+                    Vitae, maxime quod repudiandae ut possimus numquam iusto, distinctio perspiciatis provident
+                    accusamus inventore reiciendis iure, hic culpa.</h3>
+            </div>
+            <div class="course_card">
+                <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat quidem nisi cupiditate veniam?
+                    Vitae, maxime quod repudiandae ut possimus numquam iusto, distinctio perspiciatis provident
+                    accusamus inventore reiciendis iure, hic culpa.</h3>
+            </div>
+            <div class="course_card">
+                <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat quidem nisi cupiditate veniam?
+                    Vitae, maxime quod repudiandae ut possimus numquam iusto, distinctio perspiciatis provident
+                    accusamus inventore reiciendis iure, hic culpa.</h3>
+            </div>
+            <div class="course_card">
+                <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat quidem nisi cupiditate veniam?
+                    Vitae, maxime quod repudiandae ut possimus numquam iusto, distinctio perspiciatis provident
+                    accusamus inventore reiciendis iure, hic culpa.</h3>
+            </div>
+            <div class="course_card">
+                <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat quidem nisi cupiditate veniam?
+                    Vitae, maxime quod repudiandae ut possimus numquam iusto, distinctio perspiciatis provident
+                    accusamus inventore reiciendis iure, hic culpa.</h3>
+            </div>
+            <div class="course_card">
+                <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat quidem nisi cupiditate veniam?
+                    Vitae, maxime quod repudiandae ut possimus numquam iusto, distinctio perspiciatis provident
+                    accusamus inventore reiciendis iure, hic culpa.</h3>
+            </div>
+        </div>
+
+    </section>
+
+
 
 
 
@@ -29,5 +68,24 @@ export default {
 
 .founder_info {
     height: 50vh;
+}
+
+.new_courses {
+    height: 100vh;
+    background-color: green;
+
+    .courses_container {
+        width: 70%;
+        margin: auto;
+        height: 100%;
+        border: 1px dashed black;
+        background-color: red;
+        display: flex;
+        flex-wrap: wrap;
+
+        .course_card {
+            width: calc(100% / 3);
+        }
+    }
 }
 </style>

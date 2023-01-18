df643ce0784aca8992937fd5cb6d70904bf8c16b
Shelbot77 <alessiodeangelis021@gmail.com>
2023-01-18 17:08:24 +0100

    footer macrostruttura blocchi commit

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,26 @@
+<script>
+
+export default {
+    name: 'AppFooter',
+
+}
+
+
+</script>
+
+<template>
+    <footer>
+        <div class="container"></div>
+
+    </footer>
+
+
+
+</template>
+
+<style lang="scss" scoped>
+footer {
+    height: 40vh;
+    background-color: lightgreen;
+}
+</style>

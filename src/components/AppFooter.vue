ff091a820a1094bb86aa3f9b2e25ef7bd6391ddc
Shelbot77 <alessiodeangelis021@gmail.com>
2023-01-20 11:24:29 +0100

    Footer commit

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -13,28 +13,64 @@ export default {
         <div class="container d-flex">
             <div class="col-3">
                 <h3> About</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus quidem quis eum officia,
-                    accusamus consequuntur rerum commodi est illum ullam inventore ratione animi corrupti earum deleniti
-                    quia reprehenderit, incidunt iusto.</p>
+                <p>Masterstudy is Education Wordpress theme featured by Lerning Management system (LMS) for online
+                    education. <br> Developed by Stylemix Themes.
+                </p>
+                <i class="fa-brands fa-facebook"></i>
+                <i class="fa-brands fa-twitter"></i>
+                <i class="fa-brands fa-instagram"></i>
+
+
+
+
             </div>
             <div class="col-3">
-                <h3> About</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus quidem quis eum officia,
-                    accusamus consequuntur rerum commodi est illum ullam inventore ratione animi corrupti earum deleniti
-                    quia reprehenderit, incidunt iusto.</p>
+                <h3> contact</h3>
+                <p id="address">USA,California 20, First Avenue, California</p>
+                <span>Tel.+1 212 458 300 32</span>
+                <span>Fax: +1 212 375 2414</span>
+                <span id="email">info@masterstudy.com</span>
+
             </div>
             <div class="col-3">
-                <h3> About</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus quidem quis eum officia,
-                    accusamus consequuntur rerum commodi est illum ullam inventore ratione animi corrupti earum deleniti
-                    quia reprehenderit, incidunt iusto.</p>
+                <h3> pages</h3>
+                <ul class="d-flex flex-wrap">
+                    <li>Blog</li>
+                    <li>Courses</li>
+                    <li>Home</li>
+                    <li>Membership A...</li>
+                    <li>Shortcodes</li>
+                    <li>Typhograpy</li>
+
+
+
+
+
+                </ul>
+
             </div>
             <div class="col-3">
-                <h3> About</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus quidem quis eum officia,
-                    accusamus consequuntur rerum commodi est illum ullam inventore ratione animi corrupti earum deleniti
-                    quia reprehenderit, incidunt iusto.</p>
+                <h3> blog</h3>
+                <div class="posts d-flex">
+                    <img src="../assets/img/photo-1517520287167-4bbf64a00d66-50x50.jpeg" alt="">
+                    <div class="post_title">
+                        <h4>Our main target is to "Developing Youself as a Leader"</h4>
+                        <span>- August 9,2018</span>
+                    </div>
+                </div>
+                <div class="posts d-flex">
+                    <img src="../assets/img/photo-1490376840453-5f616fbebe5b-50x50.jpeg" alt="">
+                    <div class="post_title">
+                        <h4>Those Other College Expenses You Aren't Thinking About</h4>
+                        <span>- June 3,2015</span>
+                    </div>
+                </div>
+
+
+
+
             </div>
+
 
 
 
@@ -51,12 +87,62 @@ export default {
 footer {
 
     background-color: #F2B91E;
-    padding: 4rem 0;
+    padding: 3rem 0;
 
 }
 
 .col-3 {
     width: calc(100% / 4);
     padding: 1rem;
+
+    h3 {
+        text-transform: uppercase;
+    }
+
+    p {
+        line-height: 1.3;
+        margin-top: 1.5rem;
+    }
+
+    .fa-brands {
+        padding: .5rem;
+        margin-left: -.6rem;
+    }
+
+    #address {
+        margin-bottom: 1rem;
+        line-height: 1.3;
+    }
+
+    span {
+        display: block;
+    }
+
+    #email {
+        margin-top: .5rem;
+    }
+
+    ul {
+        margin-top: 1.5rem;
+
+        li {
+            width: calc(100% / 2);
+            padding: .5rem 0;
+        }
+    }
+
+    .posts {
+        margin-top: 1.5rem;
+
+        img {
+            width: 70%;
+        }
+
+        .post_title {
+            padding-left: .8rem;
+        }
+    }
+
+
 }
 </style>

e3ec33ad3c87e477147d571081116e692823e9d5
Shelbot77 <alessiodeangelis021@gmail.com>
2023-01-20 10:34:22 +0100

    Footer structure commit

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -10,7 +10,36 @@ export default {
 
 <template>
     <footer>
-        <div class="container"></div>
+        <div class="container d-flex">
+            <div class="col-3">
+                <h3> About</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus quidem quis eum officia,
+                    accusamus consequuntur rerum commodi est illum ullam inventore ratione animi corrupti earum deleniti
+                    quia reprehenderit, incidunt iusto.</p>
+            </div>
+            <div class="col-3">
+                <h3> About</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus quidem quis eum officia,
+                    accusamus consequuntur rerum commodi est illum ullam inventore ratione animi corrupti earum deleniti
+                    quia reprehenderit, incidunt iusto.</p>
+            </div>
+            <div class="col-3">
+                <h3> About</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus quidem quis eum officia,
+                    accusamus consequuntur rerum commodi est illum ullam inventore ratione animi corrupti earum deleniti
+                    quia reprehenderit, incidunt iusto.</p>
+            </div>
+            <div class="col-3">
+                <h3> About</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus quidem quis eum officia,
+                    accusamus consequuntur rerum commodi est illum ullam inventore ratione animi corrupti earum deleniti
+                    quia reprehenderit, incidunt iusto.</p>
+            </div>
+
+
+
+
+        </div>
 
     </footer>
 
@@ -20,7 +49,14 @@ export default {
 
 <style lang="scss" scoped>
 footer {
-    height: 40vh;
-    background-color: lightgreen;
+
+    background-color: #F2B91E;
+    padding: 4rem 0;
+
+}
+
+.col-3 {
+    width: calc(100% / 4);
+    padding: 1rem;
 }
 </style>
